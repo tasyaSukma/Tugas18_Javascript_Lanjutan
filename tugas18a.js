aa1ad6fce7f2e48c5b4dc7c2cebe04db04784794
tasyaSukma <maftuhah.tasya@gmail.com>
2020-09-21 10:10:35 +0700

    Add files via upload

---
+++ tugas18a.js
@@ -0,0 +1,3 @@
+var txt = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."
+var splitTxt = txt.split(" ")
+console.log(splitTxt[3] + " " + splitTxt[4])
